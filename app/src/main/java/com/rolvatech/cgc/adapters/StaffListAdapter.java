c847f09b844a05820b9a83c874327299a1a96ea5
Srinivas <srinivaslayagala@gmail.com>
2020-10-18 15:03:38 +0530

    changes

--- app/src/main/java/com/rolvatech/cgc/adapters/StaffListAdapter.java
+++ app/src/main/java/com/rolvatech/cgc/adapters/StaffListAdapter.java
@@ -1,7 +1,12 @@
 package com.rolvatech.cgc.adapters;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,14 +16,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.rolvatech.cgc.APIClient;
 import com.rolvatech.cgc.R;
 import com.rolvatech.cgc.dataobjects.StaffDTO;
 import com.rolvatech.cgc.dataobjects.UserDTO;
 import com.rolvatech.cgc.utils.FileUtils;
+import com.rolvatech.cgc.utils.PrefUtils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
+import androidx.recyclerview.widget.RecyclerView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 public class StaffListAdapter extends RecyclerView.Adapter<StaffListAdapter.ViewHolder> {
 
     List<UserDTO> staffDTOArrayList = new ArrayList<>();
@@ -39,6 +50,24 @@ public class StaffListAdapter extends RecyclerView.Adapter<StaffListAdapter.View
     @Override
     public void onBindViewHolder(@NonNull StaffListAdapter.ViewHolder holder, int position) {
         UserDTO staffDTO = staffDTOArrayList.get(position);
+        new APIClient(holder.imageView.getContext()).getApi().getProfileImage("Bearer " + PrefUtils.getStringPreference(holder.imageView.getContext(), PrefUtils.TOKEN),""+staffDTO.getId()).enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if(response.isSuccessful()){
+                    File sdcard = Environment.getDownloadCacheDirectory();
+                    String base64=response.body();
+                    String base64Image = base64.split(",")[1];
+                    Log.i("Image Async Download", "onResponse: Image String downloaded"+base64Image.length()+" file"+sdcard.getAbsolutePath()+"/cgc/images/"+staffDTO.getId()+".temp");
+                    FileUtils.writeFile(base64Image.getBytes(),sdcard.getAbsolutePath()+"/cgc/images/",staffDTO.getId()+".temp");
+                    byte[] decodedString = Base64.decode(base64Image, Base64.DEFAULT);
+                    Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                    holder.imageView.setImageBitmap(decodedByte);
+                }
+            }
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+            }
+        });
 //        if (staffDTO.getProfileImage() != null && !staffDTO.getProfileImage().equals(""))
 //            holder.imageView.setImageBitmap(FileUtils.StringToBitMap(staffDTO.getProfileImage()));
         holder.staffName.setText(staffDTO.getFirstName() + " " + staffDTO.getLastName());
@@ -56,7 +85,7 @@ public class StaffListAdapter extends RecyclerView.Adapter<StaffListAdapter.View
 
         public ViewHolder(View view) {
             super(view);
-            imageView = view.findViewById(R.id.child_image);
+            imageView = view.findViewById(R.id.staff_image);
             staffName = view.findViewById(R.id.staff_name);
             textView = view.findViewById(R.id.assigned_count);
         }
